7ddd7009782ad04784c9dc23ef51c9f535e59bcd
Ana Helena <anahelenarp@hotmail.com>
2019-03-22 19:54:41 -0300

    Tela de divulgar eventos

---
+++ src/divulgar-evento.js
@@ -0,0 +1,16 @@
+function enviarDivulgacao() {
+    var nome = $("#txtNome").val();
+    var telefone = $("#txtTelefone").val();
+    var email = $("#txtEmail").val();
+    var descricao = $("#txtDescricao").val();
+
+    if (nome === "" || nome === undefined ||
+        telefone === "" || telefone === undefined ||
+        email === "" || email === undefined ||
+        descricao === "" || descricao === undefined) {
+        
+        alert("Todos os campos são obrigatórios");
+    } else {
+        alert("Agradecemos a sua divulgação! Entraremos em contato o mais breve possível");
+    }
+}
